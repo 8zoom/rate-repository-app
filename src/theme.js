07a7ab50d501f0bb51d67c95f6e20c8e5fb32559
v <v@vs-MacBook-Pro.local>
2020-09-20 19:26:11 -0400

    10.10 platform specific font

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,5 @@
 import Constants from "expo-constants";
+import { Platform } from "react-native";
 
 const theme = {
   container: {
@@ -21,7 +22,11 @@ const theme = {
     title: 18,
   },
   fonts: {
-    main: "System",
+    main: Platform.select({
+      android: "Roboto",
+      ios: "Arial",
+      default: "System",
+    }),
   },
   fontWeights: {
     normal: "400",
