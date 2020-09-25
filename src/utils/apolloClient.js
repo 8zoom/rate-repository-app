8956c5d7717e3c0b1b0c1658fd51cb3889508945
v <v@vs-MacBook-Pro.local>
2020-09-24 21:09:22 -0400

    10.12 ENV variables with Constants.manifest.extra.env

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,8 +1,11 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient from "apollo-boost";
+import Constants from "expo-constants";
 
 const createApolloClient = () => {
+  const uri = Constants.manifest.extra.uri;
+
   return new ApolloClient({
-    uri: 'http://192.168.0.69:5000/graphql',
+    uri,
   });
 };
 
