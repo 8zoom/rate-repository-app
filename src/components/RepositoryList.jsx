6d24a57f34f920ff7c68d979241906954a4d8c2a
v <v@vs-MacBook-Pro.local>
2020-09-24 20:45:28 -0400

    10.11 fetching repos with Apollo Client

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,10 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { FlatList, View, StyleSheet, Text } from 'react-native';
 import RepositoryItem from './RepositoryItem';
-import useRepositories from '../hooks/useRepositories';
 
 import { useQuery } from '@apollo/react-hooks';
-
 import {GET_REPOSITORIES } from '../graphql/queries';
 
 
@@ -13,29 +11,17 @@ const styles = StyleSheet.create({
     height: 10,
   },
 });
-
 const ItemSeparator = () => <View style={styles.separator} />;
 
-/*
-const RepositoryList = () => {
-  return (
-    <FlatList
-      data={repositories}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={RepositoryItem}
-    />
-  );
-};
-*/
+
 
 const RepositoryList = () => {
-
   const { data, error, loading } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network'})
 
-  // if (loading)  return <Text>Loading...</Text>
-  // if (error)  return <Text>Error</Text>
-  const { repositories } = useRepositories();
+  if (loading)  return <Text>Loading...</Text>
+  if (error)  return <Text>Error</Text>
+  const { repositories } = data;
 
   // Get the nodes from the edges array
   const repositoryNodes = repositories
